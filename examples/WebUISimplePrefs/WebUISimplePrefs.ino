063eb12cdcf3cd874acb615f664b1c2c016c54c1
Souvik Chakraborty <souviksays@gmail.com>
2025-07-04 13:25:39 +0530

    Update WebUISimplePrefs.ino

--- examples/WebUISimplePrefs/WebUISimplePrefs.ino
+++ examples/WebUISimplePrefs/WebUISimplePrefs.ino
@@ -1,1 +1,16 @@
-// Placeholder for full web UI example
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <SimplePrefsPlus.h>
+
+SimplePrefsPlus prefs("config");
+ESP8266WebServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  prefs.begin();
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
